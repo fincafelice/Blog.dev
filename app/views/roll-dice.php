24e54189b204e2feabf6f17209647b8759d28f47
Felice Malaszowski <felice.vargas@gmail.com>
2015-01-29 16:35:51 -0600

    Code using laravel routes and views

---
+++ app/views/roll-dice.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+		<p><?= $computer ?></p>
+		<p><?= $guess ?></p>
+	<? if ($computer == $guess) : ?>
+		<p>Your guess matched the roll.</p>
+	<? endif; ?>
+
+</body>
+</html>

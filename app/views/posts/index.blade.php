29352f2baef3985cb4279be88c52a2684827e3c5
Felice Malaszowski <felice.vargas@gmail.com>
2015-02-03 19:22:28 -0600

    Refactored to format date/time in correct timezone and use human friendly timestamp

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -5,6 +5,8 @@
 		@foreach ($posts as $post) 
 			<h2>{{{ $post->title }}}</h2>
 			<p>{{{ $post->body }}}</p>
+			<p>{{{ $post->created_at->setTimezone('America/Chicago')->diffForHumans() }}}</p>
+
 		@endforeach
 	</ul>
 
--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -4,6 +4,8 @@
 	<ul>
 		<h2>{{{ $post->title }}}</h2>
 		<p>{{{ $post->body }}}</p>
+		<p>{{{ $post->created_at->setTimezone('America/Chicago')->diffForHumans() }}}</p>
 	</ul>	
 
-@stop
+@stop {{-- This is to view one particular post by request. --}}
+

f3b3f40968f1f91708287e6a5b9e5c5e687e0237
Felice Malaszowski <felice.vargas@gmail.com>
2015-02-03 18:49:54 -0600

    Refactored code to use Laravel Form Builder

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -1,22 +1,13 @@
 @extends('layouts.master')
 
 @section('content')    
-	<form method="POST" action="{{{ action('PostsController@store') }}}">  
     
-    <p>
-    <label for="title">Blog Title</label>
-    <input id="title" name="title" type="text" value="{{{ Input::old('title') }}}" placeholder = "Enter title!">
-    {{ $errors->first('title', '<span class="help-block">:message</span>') }}
-	</p>
-    
-    <p>
-    <label for="body">Blog Body</label><br>
-    <textarea id="body" name="body" rows="10" cols="50" placeholder = "Enter your content!">{{{ Input::old('body') }}}</textarea>
-    {{ $errors->first('body', '<span class="help-block">:message</span>') }}
-	</p>
-           
-    <button type="submit">Add</button>
-    <!-- <input type="submit"> -->
-    </form>
+{{ Form::open(array('action' => 'PostsController@store')) }}
+
+@include('posts.form')
+     
+<button type="submit">Add</button>
+
+{{ Form::close() }}
 
 @stop

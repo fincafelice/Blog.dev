207d85f4e36944c51fa01f451bf18a0d40a20711
Felice Malaszowski <felice.vargas@gmail.com>
2015-02-05 10:31:08 -0600

    Added Login/Logout functions

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -20,4 +20,40 @@ class HomeController extends BaseController {
 		return View::make('hello');
 	}
 
+	public function resume()
+	{
+		return View::make('resume');
+	}
+
+	public function portfolio()
+	{
+		return View::make('portfolio');
+	}
+
+	public function showLogin()
+	{
+		return View::make('login');
+	}
+
+	public function doLogin()
+	{
+		$email    = Input::get('email');
+		$password = Input::get('password');
+
+		if (Auth::attempt(array('email' => $email, 'password' => $password))) {
+		    return Redirect::intended('/posts');
+		} else {
+			Session::flash('errorMessage', 'Failed to log in!');
+		    return Redirect::action('HomeController@showLogin');
+		}
+	}	
+
+	public function doLogout()
+	{
+		Auth::logout();
+		Session::flash('successMessage', 'So long and thanks for all the fish!');
+		return Redirect::action('PostsController@index');
+	}
+
+
 }

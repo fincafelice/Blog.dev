53d38934fd2cbdf1a7ad9385bc0a52992e100616
Felice Malaszowski <felice.vargas@gmail.com>
2015-02-06 11:08:05 -0600

    Updated environment template.

--- env-template.php
+++ env-template.php
@@ -5,6 +5,8 @@ return array(
 	'DB_NAME' => '',
 	'DB_USER' => '',
 	'DB_PASS' => '',
+	'DB_FIRST_NAME' => '',
+	'DB_LAST_NAME' => '',
 	'DEFAULT_USER_EMAIL' => '',
 	'DEFAULT_USER_PASSWORD' => ''
 );

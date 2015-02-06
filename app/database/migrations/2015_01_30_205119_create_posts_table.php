478bcacf0ba2294345b488eb86d8a0a27ccbc735
Felice Malaszowski <felice.vargas@gmail.com>
2015-02-06 11:18:32 -0600

    Updated migration files.

--- app/database/migrations/2015_01_30_205119_create_posts_table.php
+++ app/database/migrations/2015_01_30_205119_create_posts_table.php
@@ -21,6 +21,9 @@ class CreatePostsTable extends Migration {
             $table->string('source_name', 100);
             $table->string('source_url', 200);
             $table->timestamps();
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
         });
 	}
 
